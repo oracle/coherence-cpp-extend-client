1c16b1c8f5fc5b2ac4476db0a6153891c5e7d58c
patrick.fry <patrick.fry@oracle.com>
2021-02-17 14:51:15 +0000

    Bug 32516792 - [32497946->14.1.1.0.4] CPP: listener events are being raised on NearCache get requests (main.cpp@84660 --> 14.1.1)
    [git-p4: depot-paths = "//dev/release.cpp/coherence-cpp-v14.1.1.0/": change = 84665]

--- src/coherence/component/net/extend/protocol/cache/MapEventMessage.cpp
+++ src/coherence/component/net/extend/protocol/cache/MapEventMessage.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -270,7 +270,7 @@ bool MapEventMessage::isTruncate() const
 
 void MapEventMessage::setPriming(bool fPriming)
     {
-    m_fSynthetic = fPriming;
+    m_fPriming = fPriming;
     }
 
 bool MapEventMessage::isPriming() const
