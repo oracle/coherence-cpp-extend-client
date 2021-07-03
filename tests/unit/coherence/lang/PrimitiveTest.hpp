58d33c726b20d937750b8f6001465e035428d415
patrick.fry <patrick.fry@oracle.com>
2021-07-03 06:15:34 +0000

    math.h --> cmath
    [git-p4: depot-paths = "//dev/release.cpp/coherence-cpp-v14.1.1.0/": change = 87140]

--- tests/unit/coherence/lang/PrimitiveTest.hpp
+++ tests/unit/coherence/lang/PrimitiveTest.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -11,7 +11,7 @@
 #include <iomanip>
 #include <iostream>
 #include <limits>
-#include <math.h>
+#include <cmath>
 #include <sstream>
 
 using namespace coherence::lang;
