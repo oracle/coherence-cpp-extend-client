5cf103302797ce989add07d62f42051f5ee0f591
Ryan Lubke <ryan.lubke@oracle.com>
2022-05-13 15:55:24 +0000

    BUG 34170274 - [34134018->14.1.1.0.10] Port COH-24968 - RFA: Exception occurred while encoding a AggregateAllRequest results in total disconnect for client to cpp
    [git-p4: depot-paths = "//dev/release.cpp/coherence-cpp-v14.1.1.0/": change = 92630]

---
+++ src/coherence/component/net/extend/protocol/cache/NoStorageMembers.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * https://oss.oracle.com/licenses/upl.
+ */
+#include "private/coherence/component/net/extend/protocol/cache/NoStorageMembers.hpp"
+
+#include "private/coherence/component/util/RunnableCacheEvent.hpp"
+
+#include "private/coherence/component/net/extend/RemoteNamedCache.hpp"
+
+#include "private/coherence/net/messaging/Channel.hpp"
+
+#include "coherence/util/Event.hpp"
+#include "coherence/util/Listeners.hpp"
+#include "coherence/util/MapEvent.hpp"
+
+COH_OPEN_NAMESPACE6(coherence,component,net,extend,protocol,cache)
+
+using coherence::component::net::extend::RemoteNamedCache;
+using coherence::component::util::RunnableCacheEvent;
+using coherence::net::messaging::Channel;
+using coherence::util::Event;
+using coherence::util::Listeners;
+using coherence::util::MapEvent;
+
+// ----- constructors -------------------------------------------------------
+
+NoStorageMembers::NoStorageMembers()
+    {
+    }
+
+// ----- Message interface --------------------------------------------------
+
+int32_t NoStorageMembers::getTypeId() const
+    {
+    return type_id;
+    }
+
+// ----- PortableObject interface -------------------------------------------
+
+void NoStorageMembers::readExternal(PofReader::Handle hIn)
+    {
+    NamedCacheRequest::readExternal(hIn);
+    }
+
+void NoStorageMembers::writeExternal(PofWriter::Handle hOut) const
+    {
+    NamedCacheRequest::writeExternal(hOut);
+    }
+
+// ----- internal methods ---------------------------------------------------
+
+void NoStorageMembers::onRun(AbstractPofResponse::Handle /*hResponse*/)
+    {
+    Channel::Handle          hChannel   = getChannel();
+    RemoteNamedCache::Handle hCache     = cast<RemoteNamedCache::Handle>(hChannel->getReceiver());
+    Listeners::Handle        hListeners = hCache->getDeactivationListeners();
+
+    if (!hListeners->isEmpty())
+        {
+        MapEvent::Handle hEvtDeleted = MapEvent::create(hCache, MapEvent::entry_deleted,
+                (Object::View) NULL, (Object::View) NULL, (Object::View) NULL);
+        // dispatch the event to the listeners, which are all synchronous (hence the null Queue)
+        RunnableCacheEvent::dispatchSafe(hEvtDeleted, hListeners, NULL /*Queue*/);
+        }
+    }
+
+COH_CLOSE_NAMESPACE6
