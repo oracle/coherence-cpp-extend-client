776ad5f3564b615a724d572114aff48a27d44cfb
patrick.fry <patrick.fry@oracle.com>
2025-01-11 11:04:27 -0500

    Bug 37422004 - CPP: custom address providers are not supported
    [git-p4: depot-paths = "//dev/main.cpp/": change = 113399]

---
+++ src/coherence/net/AddressProvider.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * https://oss.oracle.com/licenses/upl.
+ */
+#include "coherence/net/AddressProvider.hpp"
+
+COH_OPEN_NAMESPACE2(coherence,net)
+
+// ----- ClassLoader registration -------------------------------------------
+
+COH_REGISTER_CLASS(TypedBarrenClass<AddressProvider>::create());
+
+COH_CLOSE_NAMESPACE2
