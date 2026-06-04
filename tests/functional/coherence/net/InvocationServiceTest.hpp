e157bae64198407948f04e16637e404727df9c83
patrick.fry <patrick.fry@oracle.com>
2026-06-04 11:10:50 -0500

    Fix C++ MBean functional test management lookup
    [git-p4: depot-paths = "//dev/main.cpp/": change = 120827]

--- tests/functional/coherence/net/InvocationServiceTest.hpp
+++ tests/functional/coherence/net/InvocationServiceTest.hpp
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2026, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 #include "cxxtest/TestSuite.h"
 
@@ -82,7 +82,11 @@ class InvocationServiceTest : public CxxTest::TestSuite
         TS_ASSERT(vMap->size() == 1);
 
         String::View vs = cast<String::View>(vMap->get(vMember));
-        TS_ASSERT(vs->length() > 0);
+        TS_ASSERT(vs != NULL);
+        if (vs != NULL)
+            {
+            TS_ASSERT(vs->length() > 0);
+            }
         }
 
     /**
